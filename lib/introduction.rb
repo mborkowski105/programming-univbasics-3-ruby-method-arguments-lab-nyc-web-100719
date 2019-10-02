699bfbd9d79ebd8794f11f0703a23b36b691b517
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-02 16:32:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,9 @@ def introduction_with_language (name, language)
   return "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+def introduction_with_language (name, language = "Ruby")
+  return "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
